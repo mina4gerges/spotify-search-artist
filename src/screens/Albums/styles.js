f7d16725a8d7d6da6bc6bb6f096cef95c042c93f
mina gerges <mingerges4@gmail.com>
2021-01-29 14:51:58 +0200

    adjust center album title on xs devices

--- src/screens/Albums/styles.js
+++ src/screens/Albums/styles.js
@@ -1,12 +1,16 @@
 import {makeStyles} from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
     albumsMain: {
         width: '100%',
         height: '100%',
     },
     albumsHeader: {
-        padding: '30px 0'
+        padding: '30px 0',
+
+        [theme.breakpoints.down('xs')]: {
+            textAlign:'center'
+        }
     },
     albumsBody: {
         height: 'calc(100% - 120px)'
